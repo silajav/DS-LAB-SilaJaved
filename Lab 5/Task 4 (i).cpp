5eaf2eb31916423d6ebe8d705b7d1f7c3cc13f39
silajav <silajawed@gmail.com>
2025-09-28 05:16:27 -0700

    Create Task 4 (i).cpp

---
+++ Lab 5/Task 4 (i).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int ackermann(int m, int n) {
+    if (m == 0) {
+        return n + 1;
+    } else if (m > 0 && n == 0) {
+        return ackermann(m - 1, 1);
+    } else {
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
+}
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    cout << ackermann(m, n);
+    return 0;
+}
